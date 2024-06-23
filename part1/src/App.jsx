0beae6bb41d1be6f6066041225661aeca59d3c2c
RoniaZhang <XiaQian1999@gmail.com>
2024-06-23 17:36:07 -0400

    Part 1: Use child components for the onClick

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from 'react'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
-  
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const increaseByOne = () => setCounter(counter +1)
+  const decreaseByOne = () => setCounter(counter -1)
+  const setToZero = () => setCounter(0)
 
-  console.log('rendering... ', counter)
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='+'/>
+      <Button onClick={decreaseByOne} text='-'/>
+      <Button onClick={setToZero} text='Set to 0' />
+    </div>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
   )
 }
 export default App
