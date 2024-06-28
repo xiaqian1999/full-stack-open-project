1ee14e061f5442b221cee4f7aad2f7d4b8cc1afa
RoniaZhang <XiaQian1999@gmail.com>
2024-06-27 22:45:26 -0400

    Part 1: Array and Update of state synchronous

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,21 +1,38 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  const increaseByOne = () => setCounter(counter +1)
-  const decreaseByOne = () => setCounter(counter -1)
-  const setToZero = () => setCounter(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight + 1)
+    setTotal(left + updatedRight)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text='+'/>
-      <Button onClick={decreaseByOne} text='-'/>
-      <Button onClick={setToZero} text='Set to 0' />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
 
-const Display = ({counter}) => <div>{counter}</div>
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 export default App
