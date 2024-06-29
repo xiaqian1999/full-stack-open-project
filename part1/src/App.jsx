b5874e1f2585694536bffd54f5bc752fe4443887
RoniaZhang <XiaQian1999@gmail.com>
2024-06-28 20:20:11 -0400

    Part 1: Done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+}
+
+const Display = props => <div>{props.value}</div>
+
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const hello = (who) => () => {
-    console.log('hello, ', who)
-  }
-
   const setToValue = (newValue) => () => {
-    console.log('value now', newValue)
     setValue(newValue)
   }
     
   return (
     <div>
       {value}
-      <button onClick={hello('something')}>button</button>
-      <button onClick={setToValue(1000)}>Thousand</button>
-      <button onClick={setToValue(0)}>Reset</button>
-      <button onClick={setToValue(value + 1)}>Increment</button>
+      <Button handleClick={() => setToValue(1000)} text="thousand"/>
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
