5d982e4e1e28ed46aad6b1adffc35c3a3727c2bc
RoniaZhang <XiaQian1999@gmail.com>
2024-07-22 12:13:27 -0400

    Learn with bootstrap - Alert

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,18 +1,10 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>Hello World</Alert>
     </div>
   );
 }
